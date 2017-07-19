0e490ee722f20dbe0a67fef2ac0c3aa01927a43c
Takeshi Ueno <utaani@ueno.org>
2017-07-19 10:12:52 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,12 +24,14 @@ wget -O NotoSansCJKjp-hinted.zip https://noto-website.storage.googleapis.com/pkg
 unzip -d NotoSansCJKjp-hinted NotoSansCJKjp-hinted.zip
 sudo mv -fv ./NotoSansCJKjp-hinted/* /usr/share/fonts/opentype/noto
 rm -rfv NotoSansCJKjp-hinted.zip
+rmdir NotoSansCJKjp-hinted
 
 # NotoSerif(Mincho)
 wget -O NotoSerifCJKjp-hinted.zip https://noto-website.storage.googleapis.com/pkgs/NotoSerifCJKjp-hinted.zip
 unzip -d NotoSerifCJKjp-hinted NotoSerifCJKjp-hinted.zip
 sudo mv -fv ./NotoSerifCJKjp-hinted/* /usr/share/fonts/opentype/noto
 rm -rfv NotoSerifCJKjp-hinted.zip
+rmdir NotoSerifCJKjp-hinted
 
 # font cache
 sudo fc-cache -fv
